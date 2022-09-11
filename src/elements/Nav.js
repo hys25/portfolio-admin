22f9260c5e0895b60ffa0d0a9c63f0529a5289b1
Halyna Yavtushenko <galya.yavtushenko@gmail.com>
2022-09-11 14:21:03 -0400

    added classes for active page & fixed click area

--- src/elements/Nav.js
+++ src/elements/Nav.js
@@ -1,20 +1,22 @@
 import { Link } from "react-router-dom"
 
-function Nav({ navItems, ...props }) {
+function Nav({ navItems, activePage, ...props }) {
+  const activePageClasses = "w-full text-greyDark pr-[50px] bg-grey"
   return (
     <ul className="flex flex-col h-full mt-[50px]">
       {navItems.map((item) => (
         <li
           key={item.id}
-          className="py-4 pr-5 font-bold text-left text-white uppercase transition-all hover:w-full w-[200px] pl-[50px] mb-[10px] bg-greyDark text-14 hover:text-greyDark hover:pr-[50px] hover:bg-grey"
+          className={`font-bold text-left text-white uppercase transition-all hover:w-full  mb-[10px] bg-greyDark text-14 hover:text-greyDark hover:pr-[50px] hover:bg-grey cursor-pointer ${
+            item.slug === activePage ? activePageClasses : "w-[200px]"
+          }`}
         >
-          <Link to={item.slug}>
-            <div
-              className="flex items-center cursor-pointer text-14 hover:text-greyDark"
-              {...props}
-            >
-              {item.name}
-            </div>
+          <Link
+            to={item.slug}
+            className="flex items-center py-4 whitespace-nowrap pl-[50px] text-14 hover:text-greyDark"
+            {...props}
+          >
+            {item.name}
           </Link>
         </li>
       ))}
