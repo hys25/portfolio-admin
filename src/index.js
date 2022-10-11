cea0f6e5b3a2d9de1b7456254c98ddbda4edcc96
Halyna Yavtushenko <galya.yavtushenko@gmail.com>
2022-10-11 09:42:11 -0400

    eslint

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react"
+import { Provider } from "react-redux"
 import { BrowserRouter } from "react-router-dom"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 import { store } from "./config/store"
-import { Provider } from "react-redux"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
